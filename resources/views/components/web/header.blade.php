05299e43090882f7d45ecdfedd2090e915d81c61
laililmahfud <laililmahvut@gmail.com>
2024-02-24 10:55:37 +0700

    home page

---
+++ resources/views/components/web/header.blade.php
@@ -0,0 +1,14 @@
+<nav class="navbar bg-body-tertiary" style="border-bottom: 1px solid #ddd">
+    <div class="container">
+        <a class="navbar-brand">
+            <img src="{{ asset('assets/images/app-logo.png') }}" alt="">
+        </a>
+
+        <div class="d-flex">
+            <button class="btn btn-white" type="submit">
+               Download App
+            </button>
+            <button class="btn btn-warning" type="submit">Login</button>
+        </div>
+    </div>
+</nav>
---
+++ resources/views/web/home/index.blade.php
@@ -0,0 +1,5 @@
+<x-web.layout>
+
+     <img src="{{ asset('assets/images/bg-banner.png') }}" alt="" style="width: 100%">
+
+</x-web.layout>
